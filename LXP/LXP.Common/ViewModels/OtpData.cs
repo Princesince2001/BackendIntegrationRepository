145983a61b9c744df731e1febf530c9597ee89b1
rajkumar0828 <rajkumar08102001@gmail.com>
2024-05-08 22:07:26 +0530

    BranchEmailOTP

---
+++ LXP/LXP.Common/ViewModels/OtpData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LXP.Common.ViewModels
+{
+    public class OtpData
+    {
+        public string Otp { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ LXP/LXP.Core/Services/EmailService.cs
@@ -0,0 +1,15 @@
+﻿//using LXP.Core.IServices;
+//using LXP.Data.IRepository;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace LXP.Core.Services
+//{
+//     public class EmailService : IEmailService
+//    {
+//        private readonly ILearnerRepository _learnerRepository;
+//    }
+//}
